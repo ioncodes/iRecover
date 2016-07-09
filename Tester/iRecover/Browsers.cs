9d109ddc0265d315955cc4004f14302c56795174
ion <ionix@protonmail.com>
2016-07-09 16:11:51 +0200

    ¨Tools use now its own classfile

--- Tester/iRecover/Browsers.cs
+++ Tester/iRecover/Browsers.cs
@@ -48,30 +48,5 @@ namespace iRecover
             return passes;
         }
     }
-
-    public static class Tools
-    {
-        public static List<string> FileZilla()
-        {
-            List<string> hosts = new List<string>();
-            string DocumentoXml = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData).ToString() + "\\Filezilla\\recentservers.xml";
-            int counter = File.ReadAllText(DocumentoXml).Split(new string[] { "<Server>" }, StringSplitOptions.None).Length - 1;
-            string StrHost;
-            string StrPort;
-            string StrUser;
-            string StrPass;
-            DataSet Ds = new DataSet();
-            Ds.ReadXml(DocumentoXml);
-            for (int i = 0; i < counter; i++)
-            {
-                StrHost = Ds.Tables["Server"].Rows[i]["Host"].ToString();
-                StrPort = Ds.Tables["Server"].Rows[i]["Port"].ToString();
-                StrUser = Ds.Tables["Server"].Rows[i]["User"].ToString();
-                StrPass = Convert.FromBase64String(Ds.Tables["Server"].Rows[i]["Pass"].ToString()).ToString(); //Error wtf
-                hosts.Add("Host: " + StrHost + "\nPort: " + StrPort + "\nUser: " + StrUser + "\nPassword: " + StrPass + "\n\n");
-            }
-            return hosts;
-        }
-    }
 }
 
